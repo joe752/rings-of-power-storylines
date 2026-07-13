839fd0136126caaddcb4a1987fdf037e1e2a76ad
joe752 <darkmail752@gmail.com>
2026-07-12 18:38:50 -0700

    Remove obsolete reload episode reset

--- refresh-default.js
+++
@@ -1,8 +0,0 @@
-(() => {
-  'use strict';
-  const nav = performance.getEntriesByType?.('navigation')?.[0];
-  if (nav?.type !== 'reload') return;
-  const url = new URL(location.href);
-  url.searchParams.set('episode', '1');
-  history.replaceState(null, '', `${url.pathname}?${url.searchParams}`);
-})();
